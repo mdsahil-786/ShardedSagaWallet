5729439271b70b705de898d6b15b997a924dfb81
sahil <sahil9955555@gmail.com>
2026-03-08 23:26:54 +0530

    User Repository added

---
+++ src/main/java/com/example/shardedsagawallet/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.shardedsagawallet.repositories;
+
+import com.example.shardedsagawallet.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByNameContainingIgnoreCase(String name);
+
+}
