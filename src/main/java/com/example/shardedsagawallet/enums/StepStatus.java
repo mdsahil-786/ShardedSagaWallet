882d145265b78730ca7967497b13178b7a133b73
sahil <sahil9955555@gmail.com>
2026-03-09 07:16:28 +0530

    Enums   are  added

---
+++ src/main/java/com/example/shardedsagawallet/enums/StepStatus.java
@@ -0,0 +1,11 @@
+package com.example.shardedsagawallet.enums;
+
+public enum StepStatus {
+    PENDING,
+    RUNNING,
+    COMPLETED,
+    FAILED,
+    COMPENSATING,
+    COMPENSATED,
+    SKIPPED,
+}
