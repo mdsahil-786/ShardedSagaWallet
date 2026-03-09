a923ed25d7e041b8cd4248c167f55c413832a5dd
sahil <sahil9955555@gmail.com>
2026-03-09 17:22:21 +0530

    Repositories  are  added

---
+++ src/main/java/com/example/shardedsagawallet/repositories/SagaStepRepository.java
@@ -0,0 +1,26 @@
+package com.example.shardedsagawallet.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.shardedsagawallet.enums.StepStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.shardedsagawallet.entities.SagaStep;
+
+
+@Repository
+public interface SagaStepRepository extends JpaRepository<SagaStep, Long> {
+
+    List<SagaStep> findBySagaInstanceId(Long sagaInstanceId);
+
+    List<SagaStep> findBySagaInstanceIdAndStatus(Long sagaInstanceId, StepStatus status);
+
+    Optional<SagaStep> findBySagaInstanceIdAndStepNameAndStatus(Long sagaInstanceId, String stepName, StepStatus status);
+
+    @Query("SELECT s FROM SagaStep s WHERE s.sagaInstanceId = :sagaInstanceId AND s.status = StepStatus.COMPLETED")
+    List<SagaStep> findCompletedStepsBySagaInstanceId( @Param("sagaInstanceId") Long sagaInstanceId );
+}
